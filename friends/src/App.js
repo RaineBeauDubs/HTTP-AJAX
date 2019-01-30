72060095589209b3c48fb03fa6ca3f634b566a04
Raine Wallace <rainebeaudubs@gmail.com>
2019-01-30 15:56:40 -0500

    Got my original friends list to render to the screen.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import FriendsList from './components/FriendsList'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState({ data: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello, HTTP/AJAX!</h1>
+        {this.state.data.map(friend => (
+          <FriendsList 
+            key={friend.id}
+            friend={friend}
+          />
+        ))}
+        
       </div>
     );
   }
